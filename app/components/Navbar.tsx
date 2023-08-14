4331707d8e612c7f07eae079471288aba84a7dfa
nator <gowluonade@gmail.com>
2023-08-14 06:26:39 -0500

    publicRoutes-middleware

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,26 @@
-import { UserButton } from '@clerk/nextjs'
+'use client'
+import { UserButton, useUser } from '@clerk/nextjs'
 
-function Navbar () {
+function Navbar() {
+  const user = useUser()
+
   return (
-    <nav className='flex justify-around py-3 px-10 border-b shadow-xs border-white/20 shadow-white/20 items-center'>
-      <img src='/next.svg' alt='' className='h-5' />
-      <ul className='flex gap-6 [&>li]:cursor-pointer'>
+    <nav className="shadow-xs flex items-center justify-around border-b border-white/20 px-10 py-3 shadow-white/20">
+      <a href="/">
+        <img src="/next.svg" alt="" className="h-5" />
+      </a>
+      <ul className="flex gap-6 [&>li]:cursor-pointer">
         <li>Home</li>
         <li>History</li>
         <li>Help</li>
       </ul>
-      <UserButton />
+      {user.isSignedIn === true ? (
+        <UserButton />
+      ) : (
+        <a href="/sign-in" className="rounded-md bg-white px-4 py-2 font-bold text-black">
+          Iniciar Sesión
+        </a>
+      )}
     </nav>
   )
 }
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-export default authMiddleware({})
+export default authMiddleware({
+  publicRoutes: ['/', '/:locale/sign-in'],
+  afterAuth(auth, req, _evt) {
+    // handle users who aren't authenticated
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!auth.userId && !auth.isPublicRoute) return redirectToSignIn({ returnBackUrl: req.url })
+  }
+})
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  publicRoutes: ['/'],
+  ignoredRoutes: ['/((?!api|trpc))(_next|.+..+)(.*)', '/']
 }
